6a771cd2087246168949fe6573be481f97bf7ba3
TrainerSnow <simon.bela.bumiller@gmx.de>
2022-01-11 20:48:49 +0100

    Show Index Number For Vocab Overview

--- src/main/java/com/snow/dev/neuervokabeltrainer/ListViewAdapterVocabPair.java
+++ src/main/java/com/snow/dev/neuervokabeltrainer/ListViewAdapterVocabPair.java
@@ -1,8 +1,6 @@
 package com.snow.dev.neuervokabeltrainer;
 
-import android.app.Activity;
 import android.content.Context;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,8 +9,6 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
@@ -32,13 +28,17 @@ public class ListViewAdapterVocabPair extends ArrayAdapter<VocabPair> {
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent){
         LayoutInflater layoutInflater = LayoutInflater.from(context);
         convertView = layoutInflater.inflate(layout,parent, false);
+
         View vocabDeleteButton = convertView.findViewById(R.id.deleteVocabButton3);
         String value = getItem(position).getValue();String name = getItem(position).getName();
+
         TextView vocabNameView = convertView.findViewById(R.id.vocabName);
         TextView vocabValueView = convertView.findViewById(R.id.vocabValue);
+        TextView vocabPairIndex = convertView.findViewById(R.id.vocabPairIndex);
 
         vocabNameView.setText(name);
         vocabValueView.setText(value);
+        vocabPairIndex.setText((position + 1) + ".");
 
         vocabDeleteButton.setOnClickListener(new View.OnClickListener() {
             @Override public void onClick(View v) {
